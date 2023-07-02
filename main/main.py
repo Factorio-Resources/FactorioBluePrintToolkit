8e69de88504bbae8664f5d312582b9dfe785913c
DESKTOP-UL157US\13568 <3135351980@qq.com>
2023-07-02 17:57:32 +0800

    readme

--- main/main.py
+++ main/main.py
@@ -1,11 +1,14 @@
 from functions import *
+import sys
 
 
 def main() -> int:
-    a = '''0eNqrVkrKKU0tKMrMK4lPys/PVrKqRogUK1lFx+ooZZak5ipZIYR1wQp1lBKTSzLLUuMz81JSK5SsDHSUylKLijPz85SsjCwMTcwtjczNLI0MLIyNamsBfkciqQ==
-    '''
-    recursively_undump_the_blueprint_book_into_files(undump(a),'../test')
-    print(dump(recursively_dump_the_blueprint_book_into_files('../test/未命名蓝图薄')))
+    # 命令输入
+    command_parameters = []
+    for i in sys.argv[1:]:
+        command_parameters.append(i)
+    # 先解析
+    print(command_parameters)
     return 0
 
 
